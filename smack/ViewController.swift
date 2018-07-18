cdb8358b9a28126338a0e3105f515bbccf322c30
Adolfo <frias.adolfo@gmail.com>
2018-07-18 19:17:44 -0400

    Initial Commit

---
+++ smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by Adolfo Frias on 7/18/18.
+//  Copyright © 2018 Adolfo Frias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
