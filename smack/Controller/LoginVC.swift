fdd7d19ae4f01ac276fead726170eff213647829
Adolfo <frias.adolfo@gmail.com>
2018-07-20 16:33:06 -0400

    lesson 4

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Adolfo Frias on 7/20/18.
+//  Copyright © 2018 Adolfo Frias. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+ 
+
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Adolfo Frias on 7/20/18.
+//  Copyright © 2018 Adolfo Frias. All rights reserved.
+//
+
+import Foundation
+
+// segues
+
+let TO_LOGIN = "toLogin"
+
+
+
+
+
